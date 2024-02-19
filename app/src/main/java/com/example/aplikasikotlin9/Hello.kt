57320e71021dea1134d8ad4d3775459a58672f9f
elinakma <elinakma005@gmail.com>
2024-02-19 11:31:55 +0700

    ya

--- app/src/main/java/com/example/aplikasikotlin9/Hello.kt
+++ app/src/main/java/com/example/aplikasikotlin9/Hello.kt
@@ -1,3 +1,12 @@
 package com.example.aplikasikotlin9
 
-fun main ()
+fun main(args: Array<String>) {
+    println("Hello, ${args[0]}")
+
+    val temperature = 60
+    val isHot = if (temperature > 40) "haredang" else "dingin"
+    println("cuaca hari ini $isHot")
+
+    val isUnit = println("seperti ini ya gais")
+    println(isUnit)
+}
