0d8c3fd59f3f83756683e8bcee11cceb3c95f1a8
elinakma <elinakma005@gmail.com>
2024-02-21 16:08:46 +0700

    aplikasi kotlin 9

--- app/src/main/java/com/example/aplikasikotlin9/Hello.kt
+++ app/src/main/java/com/example/aplikasikotlin9/Hello.kt
@@ -1,12 +1,22 @@
 package com.example.aplikasikotlin9
 
 fun main(args: Array<String>) {
-    println("Hello, ${args[0]}")
+    biodata("Elin Akma Pratama", institusi = "Politeknik Negeri Madiun" )
+    hobby("Memasak","Memasak merupakan hobi saya sejak libur covid-19. Karena banyaknya aktivitas yang dilakukan di rumah, kegiatan sehari-hari saya adalah memasak. Terkadang masakan saya juga tidak enak, tetapi hal tersebut memacu saya untuk terus belajar. Sampai saat ini hobi saya adalah memasak. Saya memasak setiap hari jika terdapat waktu luang.")
+}
 
-    val temperature = 60
-    val isHot = if (temperature > 40) "haredang" else "dingin"
-    println("cuaca hari ini $isHot")
+fun biodata(nama:String, institusi:String, kelas:String = "2A") {
+    println("Nama saya adalah $nama")
+    println("Saya merupakan mahasiswa TI dari $institusi")
+    println("Saya berada di kelas $kelas")
+}
 
-    val isUnit = println("seperti ini ya gais")
-    println(isUnit)
+fun hobby(nama:String, desc:String){
+    val hobby =
+    "hobby = $nama"
+    val desc =
+    "deskripsi = $desc"
+
+    println(hobby)
+    println(desc)
 }
